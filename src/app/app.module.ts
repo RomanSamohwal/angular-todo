62d513e3b5cc9e90d2eaf827d0daf75b5fc25ed1
Roman <rsamohwal@gmail.com>
2021-12-05 00:10:21 +0300

    Добавил директиву для автофокуса

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {TodoListComponent} from "./views/todo-list/todo-list.component";
 import {FormsModule} from "@angular/forms";
+import { AutoFocusDirectiveDirective } from './directive/auto-focus-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    AutoFocusDirectiveDirective
   ],
   imports: [
     FormsModule,
